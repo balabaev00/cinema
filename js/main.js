28a39d78c4f373b5cbbd6fa47cc18b05b11beba8
balabaev00 <55126165+balabaev00@users.noreply.github.com>
2020-12-03 19:08:04 +0900

    create repository

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(function() {
+    $('.slider__items').slick({
+        arrows: false,
+        horizontal: true,
+        dots: true,
+        dotsClass: 'slider-dots',
+        autoplay: 1500,
+    });
+});
